7d0892adaaa1f0e6621769ec8871953b885bb5af
Der_Googler <jxhazhungerz@gmail.com>
2022-03-24 21:17:41 +0100

    Make fixes

--- customize.sh
+++ customize.sh
@@ -1,3 +1,16 @@
+#!/system/bin/sh
+
+if [ -n "$MMM_EXT_SUPPORT" ]; then
+  ui_print "#!useExt"
+  mmm_exec() {
+    ui_print "$(echo "#!$@")"
+  }
+else
+  mmm_exec() { true; }
+  abort "! This module need to be executed in Fox's Magisk Module Manager"
+  exit 1
+fi
+
 if [ -a /system/etc/resolv.conf ]; then
 	mkdir -p $TMPDIR/system/etc/
 	printf "nameserver 157.90.236.129\nnameserver 157.90.244.22" >> $TMPDIR/system/etc/resolv.conf
