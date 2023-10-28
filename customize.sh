932a689ec4da5f9ad26c6876adef3ef2280edffb
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2023-10-29 00:19:19 +0200

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,38 +1,7 @@
 #!/system/bin/sh
 
-if [ -n "$MMM_EXT_SUPPORT" ]; then
-  ui_print "#!useExt"
-  mmm_exec() {
-    ui_print "$(echo "#!$@")"
-  }
-else
-  mmm_exec() { true; }
-  abort "! Dieses Modul muss im Fox's Magisk Module Manager ausgefuert werden"
-  exit 1
-fi
-
-makeExecutable() {
-    chmod +x /system/bin/$@  
-}
-
-systemWrite() {
-    if [ $1 = true ]; then
-        mount -o rw,remount /    
-    elif [ $1 = false ]; then
-        mount -o ro,remount /
-    else
-        abort "System not writeable"
-    fi
-}
-
-systemWrite true
-
-makeExecutable amts-server
-
 if [ -a /system/etc/resolv.conf ]; then
 	mkdir -p $TMPDIR/system/etc/
 	printf "nameserver 157.90.236.129\nnameserver 157.90.244.22" >> $TMPDIR/system/etc/resolv.conf
 	touch $TMPDIR/auto_mount
 fi
-
-systemWrite false
